8d92b404fc3a9c95298d4e30b9db7348c03c3575
JinseongMOK <mjs4027@naver.com>
2025-05-27 19:22:31 +0900

    init: 폴더 구조 세팅

--- lib/ui/write_page/write_page.dart
+++ lib/ui/write_page/write_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class WritePage extends StatelessWidget {
+  const WritePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Text(
-          'Home',
+          'WritePage',
         ),
       ),
     );
